765be22a99b35286d4427ea6a8ca24b7aa75a9f9
Sarthak Panigrahi <sipunp70@gmail.com>
2025-05-01 09:46:16 +0530

    Add Vercel Analytics integration and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SpeechToText from "./pages/SpeechToText";
 import Home from "./pages/Home";
+import { Analytics } from "@vercel/analytics/react"; // Import Analytics
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/speech" element={<SpeechToText />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/speech" element={<SpeechToText />} />
+        </Routes>
+      </Router>
+      <Analytics /> {/* Vercel Analytics */}
+    </>
   );
 }
 
